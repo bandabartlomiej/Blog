e5121c99442af1ded8c31f7e3abf35c650641ae4
bandabartlomiej <banda.bartlomiej1@gmail.com>
2021-12-27 16:37:10 +0100

    naprawa webpacker 2

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,20 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack');
+const webpack = require('webpack')
+
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js', 'default']
 }))
 
+environment.loaders.append('jquery', {
+    test: require.resolve('jquery'),
+    use: [
+      {
+        loader: 'expose-loader',
+        options: '$',
+      },
+    ],
+  })
+  
 module.exports = environment
